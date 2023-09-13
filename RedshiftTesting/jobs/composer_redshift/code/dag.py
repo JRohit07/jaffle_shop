9150499067efde84b5d35a44885720ff08b26d07
jrohit@prophecy.io <jrohit@prophecy.io>
2023-09-13 13:51:45 +0000

    🔁 Modified     - Job        - composer_redshift

---
+++ RedshiftTesting/jobs/composer_redshift/code/dag.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import pendulum
+from datetime import timedelta
+import airflow
+from airflow import DAG
+from airflow.models.param import Param
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+from uitesting_jrohit_e2e_redshift_project_basic_composer_redshift.tasks import DBT_0
+PROPHECY_RELEASE_TAG = "__PROJECT_ID_PLACEHOLDER__/__PROJECT_RELEASE_VERSION_PLACEHOLDER__"
+
+with DAG(
+    dag_id = "uitesting_jrohit_e2e_Redshift_project_basic_composer_redshift", 
+    schedule_interval = "0 0/1 * * *", 
+    default_args = {"owner" : "Prophecy", "ignore_first_depends_on_past" : True, "do_xcom_push" : True}, 
+    start_date = pendulum.today('UTC'), 
+    catchup = True
+) as dag:
+    DBT_0_op = DBT_0()
